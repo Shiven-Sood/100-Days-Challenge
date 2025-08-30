35a2bb313d3e7a95d94f3bcb057e7b501b8983f6
Shiven Sood <ShivenSood.25509@stu.upes.ac.in>
2025-08-30 19:18:04 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h> 
+int main()
+{ 
+    int  s, a , b ; 
+    printf( " Enter the numbers ") ; 
+    scanf( " %d%d", &a, &b);
+    s = a + b;
+    printf("Sum of two numbers: %d", s);
+    return 0;
+}
