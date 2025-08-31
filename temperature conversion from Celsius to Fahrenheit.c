b9320954cf7d06e753167d5e5f101b352301596f
Shiven Sood <ShivenSood.25509@stu.upes.ac.in>
2025-08-31 20:39:47 +0530

    Temperature Conversion.c
    Converts Celsius into Fahrenheit

---
+++ temperature conversion from Celsius to Fahrenheit.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float celsius,fahrenheit;
+    printf("Enter the temorature in celsius :");
+    scanf("%f",&celsius);
+    fahrenheit = (celsius*9/5)+32 ;
+    printf("The temprature in Fahrenheit is : %f\n",fahrenheit);
+    return 0;
+}
