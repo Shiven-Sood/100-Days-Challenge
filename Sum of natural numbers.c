92632466591e8fb7653ad7b651d80af6fda90263
Shiven Sood <ShivenSood.25509@stu.upes.ac.in>
2025-09-01 10:19:59 +0530

    Create Sum of natural numbers.c
    Calculating the sum of first n natural numbers .

---
+++ Sum of natural numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{ 
+    int sum=0 , n;
+    printf("Enter the value of n :");
+    scanf("%d",&n);
+    for ( int i=1;i<=n;i++)
+    { 
+        sum = sum+i;
+    }
+    printf("The sum of first n natural numbers is = %d",sum);
+    return 0;
+}
