526902c2ff89db420008f4b1c0a9326ca05d71fe
Shiven Sood <ShivenSood.25509@stu.upes.ac.in>
2025-09-04 10:59:46 +0530

    Create Largest Number.c
    Check which is the largest number among the 3 entered numbers .

---
+++ Largest Number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a , b , c ; 
+    printf ("Enter 1st Number :") ; 
+    scanf("%d",&a);
+    printf("Enter 2nd Number :");
+    scanf("%d",&b);
+    printf("Enter 3rd Number :");
+    scanf("%d",&c);
+    if (a>=b && a >=c)
+    {
+        printf("The Largest Number is :%d",a);
+    }
+     else if (b>=a && b>=c) 
+     {
+         printf("The Largest Number is :%d",b);
+     }
+     else 
+     {
+         printf("The largest Number is :%d",c);
+     }
+}
