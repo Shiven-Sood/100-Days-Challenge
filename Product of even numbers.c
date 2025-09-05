61634862f4f625df7f42ee0d26c0c2b437dd16aa
Shiven Sood <ShivenSood.25509@stu.upes.ac.in>
2025-09-05 21:17:43 +0530

    Create Product of even numbers.c
    Calculates the product of 1st n even numbers

---
+++ Product of even numbers.c
@@ -0,0 +1,21 @@
+int main() 
+{
+    int n, i;
+    long long product = 1;   
+    int found = 0;            
+    printf("Enter n: ");
+    scanf("%d", &n);
+    printf("Even numbers from 1 to %d are: ", n);
+    for (i = 2; i <= n; i += 2) 
+    {
+        printf("%d ", i);
+        product *= i;
+        found = 1;
+    }
+
+    if (found)
+        printf("\nProduct of even numbers from 1 to %d = %lld\n", n, product);
+    else
+        printf("\nNo even numbers in the given range!\n");
+    return 0;
+}
