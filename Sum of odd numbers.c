6acecac4c59bc5bbef0d314312c0e60bd4523c6f
Shiven Sood <ShivenSood.25509@stu.upes.ac.in>
2025-09-05 21:08:31 +0530

    Create Sum of odd numbers.c
    Calculates the sum of 1st n odd numbers

---
+++ Sum of odd numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{
+    int n, i, sum = 0;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    printf("First %d odd numbers are: ", n);
+    for (i = 1; i <= n; i++) 
+    {
+        int odd = 2 * i - 1;
+        printf("%d ", odd);
+        sum += odd;
+    }
+    printf("\nSum of first %d odd numbers = %d\n", n, sum);
+    return 0;
+}
