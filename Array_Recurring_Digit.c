d01d85db8470f19d4f0b566202d3804cce882f0b
Shiven Sood <ShivenSood.25509@stu.upes.ac.in>
2025-10-12 18:30:05 +0530

    Create Array_Recurring_Digit.c

---
+++ Array_Recurring_Digit.c
@@ -0,0 +1,26 @@
+//  Find the digit that occurs the most times in an integer number.
+#include <stdio.h>
+int main() 
+{
+    int num, digit, maxDigit = 0, maxCount = 0;
+    int count[10] = {0};
+    printf("Enter an integer number: ");
+    scanf("%d", &num);
+    while (num > 0)
+{
+        digit = num % 10;
+        count[digit]++;
+        num /= 10;
+    }    
+    for (int i = 0; i < 10; i++)
+    {
+        if (count[i] > maxCount) 
+      {
+            maxCount = count[i];
+            maxDigit = i;
+        }
+    }
+
+    printf("Digit that occurs the most: %d\n", maxDigit);
+    return 0;
+}
