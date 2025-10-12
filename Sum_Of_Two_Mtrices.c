8f4bdb00d9e398f69eb110dbadde62bcbc7f2c7d
Shiven Sood <ShivenSood.25509@stu.upes.ac.in>
2025-10-12 18:52:04 +0530

    Create Sum_Of_Two_Mtrices.c

---
+++ Sum_Of_Two_Mtrices.c
@@ -0,0 +1,41 @@
+// Add two matrices.
+#include <stdio.h>
+#define MAX 100
+
+int main() {
+    int rows, cols;
+    int matrix1[MAX][MAX], matrix2[MAX][MAX], sum[MAX][MAX];
+
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter the elements of the first matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    printf("Enter the elements of the second matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sum[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+
+    printf("Output:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
