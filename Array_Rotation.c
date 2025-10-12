8990e3beb5443264ee38b21238d662b686793e30
Shiven Sood <ShivenSood.25509@stu.upes.ac.in>
2025-10-12 18:43:18 +0530

    Create Array_Rotation.c

---
+++ Array_Rotation.c
@@ -0,0 +1,33 @@
+//  Rotate an array to the right by k positions.
+#include <stdio.h>
+int main() {
+    int n, k;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the number of positions to rotate: ");
+    scanf("%d", &k);
+
+    k = k % n;
+
+    int temp[n];
+    for (int i = 0; i < n; i++) {
+        temp[(i + k) % n] = arr[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = temp[i];
+    }
+
+    printf("Rotated array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
