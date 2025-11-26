bd10c54337ff345b036f4fdc8daf2a0fa6ef6ff2
Shiven Sood <ShivenSood.25509@stu.upes.ac.in>
2025-11-26 10:37:21 +0530

    Add program to calculate max sum of subarrays
    Implement a program to find the maximum sum of subarrays of size k from an integer array.

---
+++ Program109.c
@@ -0,0 +1,54 @@
+
+// Q109: Write a program to take an integer array arr and an integer k as inputs. Print the maximum sum of all the subarrays of size k.
+
+/*
+Sample Test Cases:
+Input 1:
+arr[100, 200, 300, 400] = , k = 2
+Output 1:
+700
+
+Input 2:
+arr[1, 4, 2, 10, 23, 3, 1, 0, 20] = , k = 4
+Output 2:
+39
+
+Input 3:
+arr[100, 200, 300, 400] = , k = 1
+Output 3:
+400
+
+*/
+#include <stdio.h>
+int main() {
+    int n, k;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the value of k: ");
+    scanf("%d", &k);
+    
+    if(k > n || k <= 0) {
+        printf("Invalid value of k\n");
+        return 1;
+    }
+    int max_sum = 0;
+    for(int i = 0; i < k; i++) {
+        max_sum += arr[i];
+    }
+    int current_sum = max_sum;
+    for(int i = k; i < n; i++) {
+        current_sum += arr[i] - arr[i - k];
+        if(current_sum > max_sum) {
+            max_sum = current_sum;
+        }
+    }
+    printf("Maximum sum of subarrays of size %d is: %d\n", k, max_sum);
+    
+    return 0;
+}
